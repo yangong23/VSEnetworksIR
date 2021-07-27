4696b59541559e3d6e9575fe197bf3fb4c4aa964
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 19:31:42 +0100

    Update train_itm.py

--- UNITER/train_itm.py
+++ UNITER/train_itm.py
@@ -1,7 +1,8 @@
 # ----------------------------------------------------------------
-# Copied by Yan Gong
+# Modified by Yan Gong
 # Last revised: July 2021
-# Reference: The code is copied from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
+# Reference: The orignal code is from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
+# The code is modified to be used by VSEnetworksIR.
 # -----------------------------------------------------------------
 
 """
