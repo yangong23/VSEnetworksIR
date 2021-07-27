2ab629b34726598c01cd5fb20f81b77cf5898f07
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 16:09:49 +0100

    Update loader.py

--- UNITER/data/loader.py
+++ UNITER/data/loader.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------
 # Copied by Yan Gong
 # Last revised: July 2021
-# Reference: The code is copy from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
+# Reference: The code is copied from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
 # -----------------------------------------------------------------
 
 """
