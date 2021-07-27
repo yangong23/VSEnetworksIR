ee37bc8e0a08b2e109e3d9720bf8855b134bf4c4
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 16:21:28 +0100

    Update inf_nlvr2.py

--- UNITER/inf_nlvr2.py
+++ UNITER/inf_nlvr2.py
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------
+# Copied by Yan Gong
+# Last revised: July 2021
+# Reference: The code is copied from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
+# -----------------------------------------------------------------
+
 """run inference of NLVR2 (single GPU only)"""
 import argparse
 import json
