40c77d71824eeb1c8e6250f3ce2b261e04c6f9c3
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 20:33:05 +0100

    Update utils_itm_evaluation_and_analyseImageNetClass.py

--- UNITER/utils_itm_evaluation_and_analyseImageNetClass.py
+++ UNITER/utils_itm_evaluation_and_analyseImageNetClass.py
@@ -210,9 +210,8 @@ def itm_eval(score_matrix, txt_ids, img_ids, txt2img, img2txts):
     tr_r100 = tr_r[99]
 
     ImageNetID = [i for i in range(1000)]
-    dataframe = pd.DataFrame({'ImageNet Class ID': ImageNetID, 'Average Recall@5': Topic_r5,
-                              'Average Precision@1': Topic_p1})
-    dataframe.to_csv(Output_dir + '/UNITER_' + mode + '_Recall5_Precision1_Per_ImageNetTopic.csv', sep=',')
+    dataframe = pd.DataFrame({'ImageNet Class ID': ImageNetID, 'Image Number': Topic_im_num, 'Average Recall@5': Topic_r5, 'Average Precision@1': Topic_p1})
+    dataframe.to_csv(Output_dir + '/UNITER_' + mode + '_Recall5_Precision1_Per_ImageNetClass.csv', sep=',')
 
     dataframe = pd.DataFrame({'Query Image Name': imageNames, 'Query Image ID': imageID, 'Retrieved Results': RetrievalResult, '@1 mark': RetrievalMark})
     dataframe.to_csv(Output_dir + '/UNITER_' + mode + '_RetrievalDetail.csv', sep=',')
