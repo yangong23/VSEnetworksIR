a176e882a00098e32f05f6b92de815c0ff0136b7
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 18:52:38 +0100

    Update evaluation.py

--- VSEpp/evaluation.py
+++ VSEpp/evaluation.py
@@ -22,7 +22,7 @@ from vocab import Vocabulary
 import evaluation
 
 def main():
-        evalrank("./pretrain_model/flickr/model_best.pth.tar", data_path="/home/yan/data", split="test")
+        evalrank("$MODEL_PATH", data_path="$DATA_PATH", split="test")
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
