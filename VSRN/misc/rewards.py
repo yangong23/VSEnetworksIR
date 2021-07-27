00eaf0b87bd0bc12dab3c35407f80f067c20cd52
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 15:52:09 +0100

    Update rewards.py

--- VSRN/misc/rewards.py
+++ VSRN/misc/rewards.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------
+# Modified by Yan Gong
+# Last revised: July 2021
+# Reference: The orignal code is from VSRN: Visual Semantic Reasoning for Image-Text Matching (https://arxiv.org/pdf/1909.02701.pdf).
+# The code has been modified from python2 to python3.
+# -----------------------------------------------------------------
+
 import numpy as np
 from collections import OrderedDict
 import torch
