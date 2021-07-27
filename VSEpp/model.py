631d724f2c435d7205ff950c2a0f361293bee604
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 15:42:42 +0100

    Update model.py

--- VSEpp/model.py
+++ VSEpp/model.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------
+# Modified by Yan Gong
+# Last revised: July 2021
+# Reference: The orignal code is from VSE++: VSE++: Improving Visual-Semantic Embeddings with Hard Negatives (https://arxiv.org/abs/1707.05612).
+# The code has been modified from python2 to python3.
+# -----------------------------------------------------------------
+
 import torch
 import torch.nn as nn
 import torch.nn.init
