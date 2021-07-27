53a10c9e2b43d6ac6681536a65826b8a9fa0382e
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 16:15:01 +0100

    Update adamw.py

--- UNITER/optim/adamw.py
+++ UNITER/optim/adamw.py
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------
+# Copied by Yan Gong
+# Last revised: July 2021
+# Reference: The code is copied from UNITER: UNITER: UNiversal Image-TExt Representation Learning (https://arxiv.org/abs/1909.11740).
+# -----------------------------------------------------------------
+
 """
 AdamW optimizer (weight decay fix)
 copied from hugginface (https://github.com/huggingface/transformers).
