63e415e02071613316b2689d004416b58726b17a
Yan Gong <87724624+yangong23@users.noreply.github.com>
2021-07-27 15:38:53 +0100

    Update evaluation_retrieval_all5_and_any1.py

--- SCAN/evaluation_retrieval_all5_and_any1.py
+++ SCAN/evaluation_retrieval_all5_and_any1.py
@@ -2,7 +2,7 @@
 # On the Limitations of Visual-Semantic Embedding Networks for Image-to-Text Information Retrieval
 # by Yan Gong, Georgina Cosma, and Hui Fang
 # Programmed by Yan Gong
-# Last revised:  July 2021
+# Last revised: July 2021
 # Reference: The code is modified based on SCAN: Stacked Cross Attention for Image-Text Matching (https://arxiv.org/abs/1803.08024). 
 # Program evaluates SCAN for retrieving all 5 descriptions to the image query.
 # Copyright (c) 2021, Yan Gong. All rights reserved.
